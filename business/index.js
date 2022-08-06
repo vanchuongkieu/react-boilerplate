fa83d87fec66f0c77bcd2eb1d08673ea6f6fd803
vanchuongkieu <vanchuongkieu@gmail.com>
2022-08-06 16:05:52 +0700

    react-router-dom

--- business/index.js
+++ business/index.js
@@ -1,13 +1,17 @@
 import "@babel/polyfill";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "./src/App";
 
 const MOUNT_NODE = document.getElementById("root");
 const ROOT = ReactDOM.createRoot(MOUNT_NODE);
 ROOT.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
